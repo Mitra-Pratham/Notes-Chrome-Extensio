9eaaaf4fa02ee8dec7efe10d827a30826bfeca87
Pratham <pratham.webdev@gail.com>
2023-06-19 22:26:34 +0530

    added colors and background colors and cleaned up UI

--- notes.js
+++ notes.js
@@ -1,31 +1,31 @@
 const shortcutKeys = [
     {
         name:`Cut`,
-        keys:`Ctrl+X`,
+        keys:`Ctrl + X`,
     },
     {
         name:`Copy`,
-        keys:`Ctrl+C`,
+        keys:`Ctrl + C`,
     },
     {
         name:`Paste`,
-        keys:`Ctrl+V`,
+        keys:`Ctrl + V`,
     },
     {
         name:`Bold`,
-        keys:`Ctrl+B`,
+        keys:`Ctrl + B`,
     },
     {
         name:`Italics`,
-        keys:`Ctrl+I`,
+        keys:`Ctrl + I`,
     },
     {
         name:`Unordered List and Sub Bullets`,
-        keys:`Ctrl+]`,
+        keys:`Ctrl + ]`,
     },
     {
         name:`Ordered List and Sub Bullets`,
-        keys:`Ctrl+|`,
+        keys:`Ctrl + \\`,
     },
     {
         name:`Convert text to links`,
@@ -36,8 +36,78 @@ const shortcutKeys = [
 function createShortcuts(){
    return (
             shortcutKeys.map((el)=>{
-                return `<div class="small-paras">${el.name} - ${el.keys}</div>`
+                return `<div class="small-paras">${el.name} - <b>${el.keys}</b></div>`
             }).join(""));
+}
+
+const headingsArray = [
+    {
+        name:`Heading 1`,
+        value:`h1`,
+    },
+    {
+        name:`Heading 2`,
+        value:`h2`,
+    },
+    {
+        name:`Heading 3`,
+        value:`h3`,
+    },
+    {
+        name:`Heading 4`,
+        value:`h4`,
+    },
+    {
+        name:`Heading 5`,
+        value:`h5`,
+    },
+    {
+        name:`Heading 6`,
+        value:`h6`,
+    },
+    {
+        name:`Paragraph`,
+        value:`p`,
+    },
+]
+const colorsArray = [
+    {
+        name:`Black`,
+        value:`black`,
+    },
+    {
+        name:`White`,
+        value:`white`,
+    },
+    {
+        name:`Red`,
+        value:`red`,
+    },
+    {
+        name:`Blue`,
+        value:`blue`,
+    },
+    {
+        name:`Green`,
+        value:`green`,
+    },
+    {
+        name:`Yellow`,
+        value:`yellow`,
+    },
+]
+
+
+function createButtons(array, color){
+   return (
+            array.map((el)=>{
+                return `<button class="btn-notes-ext" value="${el.value}" title="${el.name}">
+                ${color ? colorAdd(el.value) : ''} ${color ? '' : el.name}</button>`
+            }).join(""));
+
+            function colorAdd(value){
+                return `<div value=${value} style="border:1px solid #ddd; border-radius:2px; height:14px; width:14px; background-color:${value}"></div>`
+            }
 }
 
 const minimizeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="12px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M32 416c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H32z"/></svg>`
@@ -50,7 +120,11 @@ const headingIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill=
 
 const olIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M24 56c0-13.3 10.7-24 24-24H80c13.3 0 24 10.7 24 24V176h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H40c-13.3 0-24-10.7-24-24s10.7-24 24-24H56V80H48C34.7 80 24 69.3 24 56zM86.7 341.2c-6.5-7.4-18.3-6.9-24 1.2L51.5 357.9c-7.7 10.8-22.7 13.3-33.5 5.6s-13.3-22.7-5.6-33.5l11.1-15.6c23.7-33.2 72.3-35.6 99.2-4.9c21.3 24.4 20.8 60.9-1.1 84.7L86.8 432H120c13.3 0 24 10.7 24 24s-10.7 24-24 24H32c-9.5 0-18.2-5.6-22-14.4s-2.1-18.9 4.3-25.9l72-78c5.3-5.8 5.4-14.6 .3-20.5zM224 64H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 160H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 160H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>`;
 
-const ulIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z"/></svg>`
+const ulIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z"/></svg>`;
+
+const colorsIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 448 512"><path d="M254 52.8C249.3 40.3 237.3 32 224 32s-25.3 8.3-30 20.8L57.8 416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32h-1.8l18-48H303.8l18 48H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H390.2L254 52.8zM279.8 304H168.2L224 155.1 279.8 304z"/></svg>`;
+
+const bgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 576 512"><path d="M315 315l158.4-215L444.1 70.6 229 229 315 315zm-187 5l0 0V248.3c0-15.3 7.2-29.6 19.5-38.6L420.6 8.4C428 2.9 437 0 446.2 0c11.4 0 22.4 4.5 30.5 12.6l54.8 54.8c8.1 8.1 12.6 19 12.6 30.5c0 9.2-2.9 18.2-8.4 25.6L334.4 396.5c-9 12.3-23.4 19.5-38.6 19.5H224l-25.4 25.4c-12.5 12.5-32.8 12.5-45.3 0l-50.7-50.7c-12.5-12.5-12.5-32.8 0-45.3L128 320zM7 466.3l63-63 70.6 70.6-31 31c-4.5 4.5-10.6 7-17 7H24c-13.3 0-24-10.7-24-24v-4.7c0-6.4 2.5-12.5 7-17z"/></svg>`
 
 const shortcutIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="#0d6efd" height="14px" viewBox="0 0 576 512"><path d="M64 64C28.7 64 0 92.7 0 128V384c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H64zm16 64h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80-176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V144zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zM160 336c0-8.8 7.2-16 16-16H400c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V336zM272 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM256 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM368 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM352 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V240zM464 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM448 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16z"/></svg>`
 
@@ -72,19 +146,21 @@ const init = function () {
                 <div id="formatter-row">
                 <div id="rtf-buttons">
                 <button class="btn-notes-ext headings-box" title="Headings">${headingIcon}</button>
-                    <div id="headings-box-container">
-                        <button class="btn-notes-ext" value="h1">Heading 1</button>
-                        <button class="btn-notes-ext" value="h2">Heading 2</button>
-                        <button class="btn-notes-ext" value="h3">Heading 3</button>
-                        <button class="btn-notes-ext" value="h4">Heading 4</button>
-                        <button class="btn-notes-ext" value="h5">Heading 5</button>
-                        <button class="btn-notes-ext" value="h6">Heading 6</button>
-                        <button class="btn-notes-ext" value="p">Paragraph</option>
+                    <div id="headings-box-container" class="box-ui-layout">
+                        ${createButtons(headingsArray)}
                     </div>
                     <button class="btn-notes-ext ol-box" title="Ordered List - Ctrl+|">${olIcon}</button>
                     <button class="btn-notes-ext ul-box" title="Unordered List - Ctrl+]">${ulIcon}</button>
+                    <button class="btn-notes-ext colors-box" title="Font Colors">${colorsIcon}</button>
+                        <div id="colors-box-container" class="box-ui-layout">
+                        ${createButtons(colorsArray, true)}
+                        </div>
+                    <button class="btn-notes-ext background-box" title="Background Color">${bgIcon}</button>
+                        <div id="background-box-container" class="box-ui-layout">
+                        ${createButtons(colorsArray, true)}
+                        </div>
                     <button class="btn-notes-ext shortcuts-box" title="Shortcuts">${shortcutIcon}</button>
-                    <div id="shortcuts-box-container">
+                    <div id="shortcuts-box-container" class="box-ui-layout">
                     ${createShortcuts()}
                     </div>
                 </div>
@@ -115,7 +191,7 @@ const init = function () {
 
     //get locally stored items
     chrome.storage.local.get(["textArea", "boxHeight", "boxWidth"]).then((result) => {
-        console.log(result);
+        // console.log(result);
         $('#notes-area').val(result.textArea);
         $('#notes-area').height(result.boxHeight);
         $('#notes-area').width(result.boxWidth);
@@ -151,7 +227,8 @@ const init = function () {
         $('#notes-area').append(headingElement);
         saveText();
         $('#headings-box-container').hide();
-    })
+    });
+
 
     //adding ordered list to notes area
     $('.ol-box').on('click', function (e) {
@@ -167,9 +244,43 @@ const init = function () {
         saveText();
     });
 
-    // $('.add').on('click', function (e) {
-    //     $('#notes-main').append(notesTab)
-    // });
+    //colors box toggle
+    $('.colors-box').click(function () {
+        $('#colors-box-container').toggle();
+    });
+
+      //adding color to selected font
+      $('#colors-box-container button').on('click', function(e){
+         //get cursor position
+         let sel = window.getSelection();
+         let coloredText = document.createElement('span');
+         coloredText.innerText = sel.toString();
+         coloredText.style.color = $(this).val();
+         sel.getRangeAt(0).deleteContents();
+         sel.getRangeAt(0).insertNode(coloredText);
+        saveText();
+        $('#colors-box-container').hide();
+
+    });
+
+    //background box toggle
+    $('.background-box').click(function () {
+        $('#background-box-container').toggle();
+    });
+
+     //adding background color to selected font
+     $('#background-box-container button').on('click', function(e){
+        //get cursor position
+        let sel = window.getSelection();
+        let backgroundText = document.createElement('span');
+        backgroundText.innerText = sel.toString();
+        backgroundText.style.backgroundColor = $(this).val();
+        sel.getRangeAt(0).deleteContents();
+        sel.getRangeAt(0).insertNode(backgroundText);
+       saveText();
+       $('#background-box-container').hide();
+
+   });
 
     //update on focus
     $('#notes-area').focus(function () {
@@ -213,19 +324,7 @@ const init = function () {
            }   
         }
 
-    })
-
-    // $('#height-slider').on('change', function(){
-    //     let heightChange = $('#height-slider').val();
-    //     $('#notes-area').css('height', heightChange);
-    //     saveHeight();
-    // });
-
-    // $('#width-slider').on('change', function(){
-    //     let widthChange = $('#width-slider').val();
-    //     $('#notes-area').css('width', widthChange);
-    //     saveWidth();
-    // });
+    });
 
     //top side height adjuster
     $('.notes-form-container-top').on('dragend', function (event) {
