6b95aeff2111dbdd0d18992a9286d77bca1f8699
Pratham <pratham.sethi@mitratech.com>
2023-06-23 16:03:14 +0530

    add sections + show hide sections

--- notes.js
+++ notes.js
@@ -1,113 +1,113 @@
 const shortcutKeys = [
     {
-        name:`Cut`,
-        keys:`Ctrl + X`,
+        name: `Cut`,
+        keys: `Ctrl + X`,
     },
     {
-        name:`Copy`,
-        keys:`Ctrl + C`,
+        name: `Copy`,
+        keys: `Ctrl + C`,
     },
     {
-        name:`Paste`,
-        keys:`Ctrl + V`,
+        name: `Paste`,
+        keys: `Ctrl + V`,
     },
     {
-        name:`Bold`,
-        keys:`Ctrl + B`,
+        name: `Bold`,
+        keys: `Ctrl + B`,
     },
     {
-        name:`Italics`,
-        keys:`Ctrl + I`,
+        name: `Italics`,
+        keys: `Ctrl + I`,
     },
     {
-        name:`Unordered List and Sub Bullets`,
-        keys:`Ctrl + ]`,
+        name: `Unordered List and Sub Bullets`,
+        keys: `Ctrl + ]`,
     },
     {
-        name:`Ordered List and Sub Bullets`,
-        keys:`Ctrl + \\`,
+        name: `Ordered List and Sub Bullets`,
+        keys: `Ctrl + \\`,
     },
     {
-        name:`Convert text to links`,
-        keys:`Ctrl + Shift + K`,
+        name: `Convert text to links`,
+        keys: `Ctrl + Shift + K`,
     },
 ]
 
-function createShortcuts(){
-   return (
-            shortcutKeys.map((el)=>{
-                return `<div class="small-paras">${el.name} - <b>${el.keys}</b></div>`
-            }).join(""));
+function createShortcuts() {
+    return (
+        shortcutKeys.map((el) => {
+            return `<div class="small-paras">${el.name} - <b>${el.keys}</b></div>`
+        }).join(""));
 }
 
 const headingsArray = [
     {
-        name:`Heading 1`,
-        value:`h1`,
+        name: `Heading 1`,
+        value: `h1`,
     },
     {
-        name:`Heading 2`,
-        value:`h2`,
+        name: `Heading 2`,
+        value: `h2`,
     },
     {
-        name:`Heading 3`,
-        value:`h3`,
+        name: `Heading 3`,
+        value: `h3`,
     },
     {
-        name:`Heading 4`,
-        value:`h4`,
+        name: `Heading 4`,
+        value: `h4`,
     },
     {
-        name:`Heading 5`,
-        value:`h5`,
+        name: `Heading 5`,
+        value: `h5`,
     },
     {
-        name:`Heading 6`,
-        value:`h6`,
+        name: `Heading 6`,
+        value: `h6`,
     },
     {
-        name:`Paragraph`,
-        value:`p`,
+        name: `Paragraph`,
+        value: `p`,
     },
 ]
 const colorsArray = [
     {
-        name:`Black`,
-        value:`black`,
+        name: `Black`,
+        value: `black`,
     },
     {
-        name:`White`,
-        value:`white`,
+        name: `White`,
+        value: `white`,
     },
     {
-        name:`Red`,
-        value:`red`,
+        name: `Red`,
+        value: `red`,
     },
     {
-        name:`Blue`,
-        value:`blue`,
+        name: `Blue`,
+        value: `blue`,
     },
     {
-        name:`Green`,
-        value:`green`,
+        name: `Green`,
+        value: `green`,
     },
     {
-        name:`Yellow`,
-        value:`yellow`,
+        name: `Yellow`,
+        value: `yellow`,
     },
 ]
 
 
-function createButtons(array, color){
-   return (
-            array.map((el)=>{
-                return `<button class="btn-notes-ext" value="${el.value}" title="${el.name}">
+function createButtons(array, color) {
+    return (
+        array.map((el) => {
+            return `<button class="btn-notes-ext" value="${el.value}" title="${el.name}">
                 ${color ? colorAdd(el.value) : ''} ${color ? '' : el.name}</button>`
-            }).join(""));
+        }).join(""));
 
-            function colorAdd(value){
-                return `<div value=${value} style="border:1px solid #ddd; border-radius:2px; height:14px; width:14px; background-color:${value}"></div>`
-            }
+    function colorAdd(value) {
+        return `<div value=${value} style="border:1px solid #ddd; border-radius:2px; height:14px; width:14px; background-color:${value}"></div>`
+    }
 }
 
 const minimizeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="12px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M32 416c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H32z"/></svg>`
@@ -125,6 +125,10 @@ const ulIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6
 const colorsIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 448 512"><path d="M254 52.8C249.3 40.3 237.3 32 224 32s-25.3 8.3-30 20.8L57.8 416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32h-1.8l18-48H303.8l18 48H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H390.2L254 52.8zM279.8 304H168.2L224 155.1 279.8 304z"/></svg>`;
 
 const bgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 576 512"><path d="M315 315l158.4-215L444.1 70.6 229 229 315 315zm-187 5l0 0V248.3c0-15.3 7.2-29.6 19.5-38.6L420.6 8.4C428 2.9 437 0 446.2 0c11.4 0 22.4 4.5 30.5 12.6l54.8 54.8c8.1 8.1 12.6 19 12.6 30.5c0 9.2-2.9 18.2-8.4 25.6L334.4 396.5c-9 12.3-23.4 19.5-38.6 19.5H224l-25.4 25.4c-12.5 12.5-32.8 12.5-45.3 0l-50.7-50.7c-12.5-12.5-12.5-32.8 0-45.3L128 320zM7 466.3l63-63 70.6 70.6-31 31c-4.5 4.5-10.6 7-17 7H24c-13.3 0-24-10.7-24-24v-4.7c0-6.4 2.5-12.5 7-17z"/></svg>`
+
+const addSectionsIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 512 512"><path d="M512 416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64V416zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24V312h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H280V200c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H168c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z"/></svg>`;
+
+const showSectionsIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="14px" fill="#0d6efd" viewBox="0 0 576 512"><path d="M384 480h48c11.4 0 21.9-6 27.6-15.9l112-192c5.8-9.9 5.8-22.1 .1-32.1S555.5 224 544 224H144c-11.4 0-21.9 6-27.6 15.9L48 357.1V96c0-8.8 7.2-16 16-16H181.5c4.2 0 8.3 1.7 11.3 4.7l26.5 26.5c21 21 49.5 32.8 79.2 32.8H416c8.8 0 16 7.2 16 16v32h48V160c0-35.3-28.7-64-64-64H298.5c-17 0-33.3-6.7-45.3-18.7L226.7 50.7c-12-12-28.3-18.7-45.3-18.7H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H87.7 384z"/></svg>`
 
 const shortcutIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="#0d6efd" height="14px" viewBox="0 0 576 512"><path d="M64 64C28.7 64 0 92.7 0 128V384c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H64zm16 64h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80-176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V144zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zM160 336c0-8.8 7.2-16 16-16H400c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V336zM272 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM256 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM368 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM352 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V240zM464 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM448 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16z"/></svg>`
 
@@ -149,7 +153,7 @@ const init = function () {
                     <div id="headings-box-container" class="box-ui-layout">
                         ${createButtons(headingsArray)}
                     </div>
-                    <button class="btn-notes-ext ol-box" title="Ordered List - Ctrl+|">${olIcon}</button>
+                    <button class="btn-notes-ext ol-box" title="Ordered List - Tab">${olIcon}</button>
                     <button class="btn-notes-ext ul-box" title="Unordered List - Ctrl+]">${ulIcon}</button>
                     <button class="btn-notes-ext colors-box" title="Font Colors">${colorsIcon}</button>
                         <div id="colors-box-container" class="box-ui-layout">
@@ -158,6 +162,10 @@ const init = function () {
                     <button class="btn-notes-ext background-box" title="Background Color">${bgIcon}</button>
                         <div id="background-box-container" class="box-ui-layout">
                         ${createButtons(colorsArray, true)}
+                        </div>
+                    <button class="btn-notes-ext add-sections-box" title="Add Sections">${addSectionsIcon}</button>
+                    <button class="btn-notes-ext show-sections-box" title="Show/Hide Sections">${showSectionsIcon}</button>
+                        <div id="show-sections-box-container" class="box-ui-layout">
                         </div>
                     <button class="btn-notes-ext shortcuts-box" title="Shortcuts">${shortcutIcon}</button>
                     <div id="shortcuts-box-container" class="box-ui-layout">
@@ -195,9 +203,35 @@ const init = function () {
         $('#notes-area').val(result.textArea);
         $('#notes-area').height(result.boxHeight);
         $('#notes-area').width(result.boxWidth);
-        $('#height-slider').val(result.boxHeight);
-        $('#width-slider').val(result.boxWidth);
+        // $('#height-slider').val(result.boxHeight);
+        // $('#width-slider').val(result.boxWidth);
     });
+
+    function createSections() {
+        let sectionsAreaArray = document.getElementsByClassName('sections-area');
+        let sectionsList = [];
+        for (let i = 0; i < sectionsAreaArray.length; i++) {
+            let tempID = document.getElementsByClassName('sections-area')[i].id;
+            let tempText = document.getElementsByClassName('sections-area')[i].innerText;
+            let tempDisplay = document.getElementsByClassName('sections-area')[i].style.display;
+            let checkSection = `<div>
+                        <input class="${tempID}" name="${tempID}" type="checkbox" ${tempDisplay === 'none' ? '' : 'checked'}>
+                        <label for="${tempID}"> ${tempText.substring(0, 20)}...</label>
+            </div>`
+            sectionsList.push(checkSection);
+        }
+        $('#show-sections-box-container').empty();
+        $('#show-sections-box-container').append(sectionsList);
+        for (let i = 0; i < document.getElementsByClassName('sections-area').length; i++) {
+            let tempID = document.getElementsByClassName('sections-area')[i].id;
+            $(`.${tempID}`).on('click', function () {
+                $(`#${tempID}`).toggle();
+                saveText();
+                $('.show-sections-box').toggleClass('btn-notes-ext-active');
+                $('#show-sections-box-container').toggle();
+            });
+        }
+    }
 
     //minimize box
     $('.min-box').on('click', function (e) {
@@ -208,6 +242,7 @@ const init = function () {
     $('.max-box').on('click', function (e) {
         $(e.target).offsetParent('.notes-header').find('.notes-form').show();
         getLocalStore('text', 'height', 'width');
+        // createSections();
     });
 
     //close box
@@ -217,14 +252,14 @@ const init = function () {
 
     //headings box toggle
     $('.headings-box').click(function () {
-        $('.headings-box').addClass('btn-notes-ext-active');
+        $('.headings-box').toggleClass('btn-notes-ext-active');
         $('#headings-box-container').toggle();
     });
 
     //adding headings to notes area
-    $('#headings-box-container button').on('click', function(e){
+    $('#headings-box-container button').on('click', function (e) {
         let heading = e.target.value;
-        let headingElement = `<${heading}>Heading ${heading.slice(1,2)}</${heading}>`;
+        let headingElement = `<${heading}>Heading ${heading.slice(1, 2)}</${heading}>`;
         $('#notes-area').append(headingElement);
         saveText();
         $('#headings-box-container').hide();
@@ -248,19 +283,19 @@ const init = function () {
 
     //colors box toggle
     $('.colors-box').click(function () {
-        $('.colors-box').addClass('btn-notes-ext-active');
+        $('.colors-box').toggleClass('btn-notes-ext-active');
         $('#colors-box-container').toggle();
     });
 
-      //adding color to selected font
-      $('#colors-box-container button').on('click', function(e){
-         //get cursor position
-         let sel = window.getSelection();
-         let coloredText = document.createElement('span');
-         coloredText.innerText = sel.toString();
-         coloredText.style.color = $(this).val();
-         sel.getRangeAt(0).deleteContents();
-         sel.getRangeAt(0).insertNode(coloredText);
+    //adding color to selected font
+    $('#colors-box-container button').on('click', function (e) {
+        //get cursor position
+        let sel = window.getSelection();
+        let coloredText = document.createElement('span');
+        coloredText.innerText = sel.toString();
+        coloredText.style.color = $(this).val();
+        sel.getRangeAt(0).deleteContents();
+        sel.getRangeAt(0).insertNode(coloredText);
         saveText();
         $('#colors-box-container').hide();
         $('.colors-box').removeClass('btn-notes-ext-active');
@@ -269,12 +304,12 @@ const init = function () {
 
     //background box toggle
     $('.background-box').click(function () {
-        $('.background-box').addClass('btn-notes-ext-active');
+        $('.background-box').toggleClass('btn-notes-ext-active');
         $('#background-box-container').toggle();
     });
 
-     //adding background color to selected font
-     $('#background-box-container button').on('click', function(e){
+    //adding background color to selected font
+    $('#background-box-container button').on('click', function (e) {
         //get cursor position
         let sel = window.getSelection();
         let backgroundText = document.createElement('span');
@@ -282,37 +317,55 @@ const init = function () {
         backgroundText.style.backgroundColor = $(this).val();
         sel.getRangeAt(0).deleteContents();
         sel.getRangeAt(0).insertNode(backgroundText);
-       saveText();
-       $('#background-box-container').hide();
-       $('.background-box').removeClass('btn-notes-ext-active');
+        saveText();
+        $('#background-box-container').hide();
+        $('.background-box').removeClass('btn-notes-ext-active');
 
-   });
+    });
+
+    //show sections box toggle
+    $('.show-sections-box').click(function () {
+        $('.show-sections-box').toggleClass('btn-notes-ext-active');
+        $('#show-sections-box-container').toggle();
+    });
+
+    
+    //adding sections to notes area
+    $('.add-sections-box').on('click', function (e) {
+        let randomID = `sections-area-${Math.round(Math.random() * 90000000)}`
+        let sectionsList = `
+     <div id="${randomID}" class="sections-area"><h2>New Section</h2></div>`
+        $('#notes-area').append(sectionsList);
+        saveText();
+        createSections();
+    });
 
     //update on focus
     $('#notes-area').focus(function () {
         getLocalStore('text', 'height', 'width');
     });
 
-    //save on focus out
+    // save on focus out
     $('#notes-area').blur(function () {
         saveText();
+        createSections();
     });
 
     //keypress listener here
-    $('#notes-area').on('keypress', function (event) {
-        // console.log(event.keyCode);
-        // shortcut ctrl+]
-        if (event.ctrlKey && event.keyCode === 29) {
+    $('#notes-area').on('keydown', function (event) {
+        // shortcut tab
+        if (event.keyCode === 9) {
+            event.preventDefault();
             //get cursor position - unordered lists
-           let sel = window.getSelection();
+            let sel = window.getSelection();
             let ulElement = document.createElement('ul');
             ulElement.innerHTML = '<li>An item here</li>'
             sel.getRangeAt(0).insertNode(ulElement);
         }
         // shortcut ctrl+\ - ordered lists
-         if (event.ctrlKey && event.keyCode === 28) {
+        if (event.ctrlKey && event.keyCode === 28) {
             //get cursor position
-           let sel = window.getSelection();
+            let sel = window.getSelection();
             let olElement = document.createElement('ol');
             olElement.innerHTML = '<li>An item here</li>'
             sel.getRangeAt(0).insertNode(olElement);
@@ -320,14 +373,14 @@ const init = function () {
         // shortcut ctrl+shift+k - adding links
         if (event.ctrlKey && event.keyCode === 11) {
             //get cursor position
-           let sel = window.getSelection();
+            let sel = window.getSelection();
             let anchorTag = document.createElement('span');
-           let anchorLink = prompt('please enter URL here','https://google.com');
-           if(anchorLink){
-            anchorTag.innerHTML = `<a href=${anchorLink} target="_blank">${sel.toString()}</a>`
-            sel.getRangeAt(0).deleteContents();
-            sel.getRangeAt(0).insertNode(anchorTag);
-           }   
+            let anchorLink = prompt('please enter URL here', 'https://google.com');
+            if (anchorLink) {
+                anchorTag.innerHTML = `<a href=${anchorLink} target="_blank">${sel.toString()}</a>`
+                sel.getRangeAt(0).deleteContents();
+                sel.getRangeAt(0).insertNode(anchorTag);
+            }
         }
 
     });
@@ -378,6 +431,8 @@ const init = function () {
             text ? $('#notes-area').html(result.textArea) : '';
             height ? $('#notes-area').height(result.boxHeight) : '';
             width ? $('#notes-area').width(result.boxWidth) : '';
+            createSections();
+
         });
     }
 
